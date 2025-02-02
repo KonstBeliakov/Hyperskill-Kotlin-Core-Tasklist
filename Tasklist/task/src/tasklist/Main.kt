b35f257d2fdfd2bff6affa74db4c0d5fa5d9ca4e
KonstBeliakov <konstbeliakov@gmail.com>
2025-02-02 17:38:10 +0300

    added date, time and priority of tasks

--- Tasklist/task/src/tasklist/Main.kt
+++ Tasklist/task/src/tasklist/Main.kt
@@ -1,7 +1,95 @@
 package tasklist
 
+import java.time.LocalDate
+import java.time.LocalTime
+
+enum class Priority(val shortName: String) {
+    CRITICAL("C"),
+    HIGH("H"),
+    NORMAL("N"),
+    LOW("L");
+
+    companion object {
+        fun fromString(value: String): Priority? {
+            return enumValues<Priority>().find { it.shortName == value }
+        }
+    }
+}
+
+class Task(val lines: MutableList<String>, val priority: Priority, val date: LocalDate, val time: LocalTime)
+
+
+fun stringToDate(date: String): LocalDate? {
+    try {
+        val (year, month, day) = date.split("-").map { it.toInt() }
+        return LocalDate.of(year, month, day)
+    } catch (e: Exception) {
+        return null
+    }
+
+}
+
+fun stringToTime(time: String): LocalTime? {
+    try {
+        val (hour, minute) = time.split(":").map { it.toInt() }
+        return LocalTime.of(hour, minute, 0, 0)
+    } catch (e: Exception) {
+        return null
+    }
+}
+
+fun addTask(tasks: MutableList<Task>) {
+    println("Input the task priority (C, H, N, L):")
+    val priority = Priority.fromString(readln().uppercase())
+
+    println("Input the date (yyyy-mm-dd):")
+    var dateString = readln()
+    while (stringToDate(dateString) == null) {
+        println("The input date is invalid")
+        dateString = readln()
+    }
+    val date = stringToDate(dateString)
+
+    println("Input the time (hh:mm):")
+
+    var timeString = readln()
+    while (stringToTime(timeString) == null) {
+        println("The input time is invalid")
+        timeString = readln()
+    }
+    val time = stringToTime(timeString)
+
+    println("Input a new task (enter a blank line to end):")
+    var line = readln()
+    val lines: MutableList<String> = mutableListOf()
+    while (line.isNotEmpty()) {
+        lines.add(line.trim())
+        line = readln()
+    }
+
+    if (lines.isEmpty())
+        println("The task is blank")
+    else
+        tasks.add(Task(lines, priority!!, date!!, time!!))
+}
+
+fun printTasksInfo(tasks: MutableList<Task>) {
+    val indent = tasks.size.toString().length + 1
+    val indentPrefix = " ".repeat(indent)
+
+    tasks.forEachIndexed { index, task ->
+        print((index + 1).toString().padEnd(indent, ' '))
+        println("${task.date} ${task.time} ${task.priority.shortName}")
+
+        task.lines.forEach { line ->
+            println("$indentPrefix$line")
+        }
+        println()
+    }
+}
+
 fun main() {
-val tasks: MutableList<MutableList<String>> = mutableListOf()
+    val tasks: MutableList<Task> = mutableListOf()
 
     var end = false
 
@@ -9,31 +97,9 @@ val tasks: MutableList<MutableList<String>> = mutableListOf()
         println("Input an action (add, print, end):")
         val command = readln()
         when (command) {
-            "add" -> {
-                println("Input a new task (enter a blank line to end):")
-                var line = readln()
-                val task: MutableList<String> = mutableListOf()
-                while (line.isNotEmpty()) {
-                    task.add(line.trim())
-                    line = readln()
-                }
-                tasks.add(task)
-            }
+            "add" -> addTask(tasks)
 
-            "print" -> {
-                val indent = tasks.size.toString().length + 1
-                val indentPrefix = " ".repeat(indent)
-
-                tasks.forEachIndexed{ index, task ->
-                    print((index + 1).toString().padEnd(indent, ' '))
-                    println(task.first())
-
-                    task.drop(1).forEach{ line ->
-                        println("$indentPrefix$line")
-                    }
-                    println()
-                }
-            }
+            "print" -> printTasksInfo(tasks)
 
             "end" -> {
                 println("Tasklist exiting!")
