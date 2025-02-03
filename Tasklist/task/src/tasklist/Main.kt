5573c3ac8aaf44ce1a0c9a5637180d0a26da5af9
KonstBeliakov <konstbeliakov@gmail.com>
2025-02-03 11:47:36 +0300

    Added ability to edit and delete tasks

--- Tasklist/task/src/tasklist/Main.kt
+++ Tasklist/task/src/tasklist/Main.kt
@@ -16,7 +16,7 @@ enum class Priority(val shortName: String) {
     }
 }
 
-class Task(val lines: MutableList<String>, val priority: Priority, val date: LocalDate, val time: LocalTime)
+class Task(var lines: MutableList<String>, var priority: Priority, var date: LocalDate, var time: LocalTime)
 
 
 fun stringToDate(date: String): LocalDate? {
@@ -38,18 +38,22 @@ fun stringToTime(time: String): LocalTime? {
     }
 }
 
-fun addTask(tasks: MutableList<Task>) {
+fun readPriority(): Priority {
     println("Input the task priority (C, H, N, L):")
-    val priority = Priority.fromString(readln().uppercase())
+    return Priority.fromString(readln().uppercase())!!
+}
 
+fun readDate(): LocalDate {
     println("Input the date (yyyy-mm-dd):")
     var dateString = readln()
     while (stringToDate(dateString) == null) {
         println("The input date is invalid")
         dateString = readln()
     }
-    val date = stringToDate(dateString)
+    return stringToDate(dateString)!!
+}
 
+fun readTime(): LocalTime {
     println("Input the time (hh:mm):")
 
     var timeString = readln()
@@ -57,8 +61,10 @@ fun addTask(tasks: MutableList<Task>) {
         println("The input time is invalid")
         timeString = readln()
     }
-    val time = stringToTime(timeString)
+    return stringToTime(timeString)!!
+}
 
+fun readLines(): MutableList<String> {
     println("Input a new task (enter a blank line to end):")
     var line = readln()
     val lines: MutableList<String> = mutableListOf()
@@ -67,10 +73,19 @@ fun addTask(tasks: MutableList<Task>) {
         line = readln()
     }
 
+    return lines
+}
+
+fun addTask(tasks: MutableList<Task>) {
+    val priority = readPriority()
+    val date = readDate()
+    val time = readTime()
+    val lines = readLines()
+
     if (lines.isEmpty())
         println("The task is blank")
     else
-        tasks.add(Task(lines, priority!!, date!!, time!!))
+        tasks.add(Task(lines, priority, date, time))
 }
 
 fun printTasksInfo(tasks: MutableList<Task>) {
@@ -88,18 +103,68 @@ fun printTasksInfo(tasks: MutableList<Task>) {
     }
 }
 
+fun chooseTask(tasks: MutableList<Task>): Int? {
+    if (tasks.isEmpty()) {
+        println("No tasks have been input")
+        return null
+    } else {
+        printTasksInfo(tasks)
+        println("Input the task number (1-${tasks.size}):")
+        var taskNumber = readln().toIntOrNull()
+        while (taskNumber == null || taskNumber !in 1..tasks.size) {
+            println("Invalid task number")
+            taskNumber = readln().toIntOrNull()
+        }
+        return taskNumber - 1
+    }
+}
+
+fun editTask(tasks: MutableList<Task>) {
+    val taskNumber = chooseTask(tasks)
+    taskNumber?.let {
+        println("Input a field to edit (priority, date, time, task):")
+        var field = readln()
+        while (field !in listOf("priority", "date", "time", "task")) {
+            println("Invalid field")
+            field = readln()
+        }
+
+        tasks[taskNumber].apply {
+            when (field) {
+                "priority" -> priority = readPriority()
+                "date" -> date = readDate()
+                "time" -> time = readTime()
+                "task" -> lines = readLines()
+            }
+        }
+        println("The task is changed")
+    }
+}
+
+fun deleteTask(tasks: MutableList<Task>) {
+    val taskNumber = chooseTask(tasks)
+    if (taskNumber != null) {
+        tasks.removeAt(taskNumber - 1)
+        println("The task is deleted")
+    }
+}
+
 fun main() {
     val tasks: MutableList<Task> = mutableListOf()
 
     var end = false
 
     while (!end) {
-        println("Input an action (add, print, end):")
+        println("Input an action (add, print, edit, delete, end):")
         val command = readln()
         when (command) {
             "add" -> addTask(tasks)
 
             "print" -> printTasksInfo(tasks)
+
+            "edit" -> editTask(tasks)
+
+            "delete" -> deleteTask(tasks)
 
             "end" -> {
                 println("Tasklist exiting!")
